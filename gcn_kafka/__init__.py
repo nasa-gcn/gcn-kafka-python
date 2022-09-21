3ec41a3af853feccb5a03aa72b2671ca258b2195
Leo Singer <leo.singer@ligo.org>
2022-09-21 11:59:49 -0400

    Add AdminClient

--- gcn_kafka/__init__.py
+++ gcn_kafka/__init__.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: CC0-1.0
 
-from .core import Consumer, Producer
+from .core import Consumer, Producer, AdminClient
 from .env import config_from_env
 from ._version import version as __version__
 
-__all__ = ("config_from_env", "Consumer", "Producer")
+__all__ = ("config_from_env", "Consumer", "Producer", "AdminClient")
